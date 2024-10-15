286027620280db362e460f488b143c19f33aeb4f
Ekaterina Briuchanova <ueobrn@yandex.ru>
2024-10-15 16:54:53 +0700

    add ecept test

--- src/test/java/PracticumTest/BurgerTest.java
+++ src/test/java/PracticumTest/BurgerTest.java
@@ -8,8 +8,10 @@ import org.mockito.junit.MockitoJUnitRunner;
 import praktikum.Bun;
 import praktikum.Burger;
 import praktikum.Ingredient;
+import praktikum.IngredientType;
 
 import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.when;
 
 @RunWith(MockitoJUnitRunner.class)
 public class BurgerTest {
@@ -57,12 +59,44 @@ burger.moveIngredient(1,2);
     //проверяем, что ingredient2 теперь находится на позиции 1.
     assertEquals(ingredient2, burger.ingredients.get(1));
 }
-//
-//    public void removeIngredient(int index) {
-//        ingredients.remove(index);
-//    }
-//
-//    public void moveIngredient(int index, int newIndex) {
-//        ingredients.add(newIndex, ingredients.remove(index));
-//    }
+@Test
+public void burgerGetPriceTest(){
+burger.setBuns(bun);
+burger.addIngredient(ingredient);
+burger.addIngredient(ingredient1);
+//настоим саб
+    when(bun.getPrice()).thenReturn(2.5F);
+    when(ingredient.getPrice()).thenReturn(1.5f);
+    when(ingredient1.getPrice()).thenReturn(1.1f);
+//расчитаем цену по формуле
+  float ex= (2.5f*2)+1.5f+1.1f;
+   float actualPrice =burger.getPrice();
+   assertEquals("Стоимость должна совпадать ",ex,actualPrice,0.1);
+}
+@Test
+    public void getBurgerReceptTest(){
+    //настроим саб
+    when(bun.getName()).thenReturn("classic");
+    when(bun.getPrice()).thenReturn(1.5F);
+    when(ingredient.getName()).thenReturn("cheese cheddar");
+    when(ingredient.getPrice()).thenReturn(3.5f);
+    when(ingredient.getType()).thenReturn(IngredientType.SAUCE);
+    //добавим булочку и ингредиенты соберем бургер
+    burger.setBuns(bun);
+    burger.addIngredient(ingredient);
+   //ожидаемый результат
+    float totalPrice = 1.5f + 3.5f + 1.5f;
+String exp= String.format( "(==== %s ====)%n" +
+                "= %s %s =%n" +
+                "(==== %s ====)%n" +
+                "%nPrice: %.6f%n", //  %.1f для соответствия фактическому выводу(6знаков в ожидаемом)
+        "classic",
+        "sauce",
+        "cheese cheddar",
+        "classic",
+    totalPrice);
+    String actualRecept =burger.getReceipt();
+assertEquals(exp, actualRecept);
+}
+
 }
